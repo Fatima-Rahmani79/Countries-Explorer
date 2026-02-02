f366a416331e12afd143b6b0ddae309abe985e93
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-02-02 10:28:03 +0430

    complete header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Countries Explorer</h1>
+      <h3>Discover countries around the world</h3>
+    </header>
+  );
+}
